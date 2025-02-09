6e6b2c72f2b7169ad18ba4eb668ce105b5f6edaf
Ana <anapedrabcn@gmail.com>
2025-02-09 14:19:46 +0100

    added my links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import UrlProvider from "./context";
+
 import AppLayout from "./layouts/app-layout";
-import Dashboard from "./pages/dashboard";
-import Auth from "./pages/auth";
+import RequireAuth from "./components/require-auth";
+
 import RedirectLink from "./pages/redirect-link";
 import LandingPage from "./pages/landing";
+import Dashboard from "./pages/dashboard";
+import LinkPage from "./pages/link";
+import Auth from "./pages/auth";
 
 const router = createBrowserRouter([
   {
@@ -15,16 +20,24 @@ const router = createBrowserRouter([
         element: <LandingPage />,
       },
       {
-        path: "/dashboard",
-        element: <Dashboard />,
-      },
-      {
         path: "/auth",
         element: <Auth />,
       },
       {
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
+      },
+      {
         path: "/link/:id",
-        element: <Link />,
+        element: (
+          <RequireAuth>
+            <LinkPage />
+          </RequireAuth>
+        ),
       },
       {
         path: "/:id",
@@ -35,7 +48,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
 }
 
 export default App;
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -10,22 +10,38 @@ import {
 } from "@/components/ui/accordion";
 
 const LandingPage = () => {
+  const [longUrl, setLongUrl] = useState("");
+  const navigate = useNavigate();
+
+  const handleShorten = (e) => {
+    e.preventDefault();
+    if (longUrl) navigate(`/auth?createNew=${longUrl}`);
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
         The only URL Shortener <br /> you&rsquo;ll ever need! 👇
       </h2>
-      <form className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2">
+      <form
+        onSubmit={handleShorten}
+        className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2"
+      >
         <Input
           type="url"
           placeholder="Enter your loooong URL"
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
           className="h-full flex-1 py-4 px-4"
         />
-        <Button className="h-full" type="submit" variant="destructive">
+        <Button type="submit" className="h-full" variant="destructive">
           Shorten!
         </Button>
       </form>
-      <img src="/banner.jpeg" alt="banner" className="w-full my-11 md:px-11" />
+      <img
+        src="/banner1.jpg" // replace with 2 in small screens
+        className="w-full my-11 md:px-11"
+      />
       <Accordion type="multiple" collapsible className="w-full md:px-11">
         <AccordionItem value="item-1">
           <AccordionTrigger>
